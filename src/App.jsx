c17f906def1699fa2d15272092babce82d7603a9
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-25 21:06:38 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 //styles
 import "./App.css"
+
 //Components
+import Navbar from "./components/Navbar/Navbar"
+import HomeSlider from "./components/HomeSlider/HomeSlider"
+import StatsBox from "./components/Statsbox/StatsBox"
+import Footer from "./components/footer/Footer"
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <main>
+    <Navbar/>
+    <HomeSlider/>
+    <StatsBox/>
+    <Footer/>
+    </main>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//css
+import styles from "./Navbar.module.css"
+const Navbar = () => {
+    return (
+        <nav>
+           Navbar 
+        </nav>
+    )
+}
+
+export default Navbar
