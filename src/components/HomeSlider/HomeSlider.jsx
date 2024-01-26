8ed1bb1549215457c8bccb5aa71906c8f4522439
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-26 23:25:15 +0500

    main

--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Swiper, SwiperSlide } from "swiper/react"
-//css
-import styles from "./HomeSlider.module.css"
-//Static Import Array
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; // Import Swiper styles
+import styles from './HomeSlider.module.css';
+
+// Static Import Array
+const ImageSlider = ['./swiperslide.jpg', './swiperslide', './swiperslide.jpg'];
 
 const HomeSlider = () => {
   return (
-    <Swiper className={styles.SliderContainer}>
-      
+<div className={styles.MainContainer}>
+<Swiper className={styles.SliderContainer}>
+      {ImageSlider.map((image, index) => (
+        <SwiperSlide className={styles.SlideItem} key={index}>
+          <img src={image} alt={`Slide ${index + 1}`} />
+        </SwiperSlide>
+      ))}
     </Swiper>
-  )
-}
+</div>
+  );
+};
 
-export default HomeSlider
+export default HomeSlider;
