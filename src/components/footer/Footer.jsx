353e4cf2e8ef8842ead302819625dcb28eaba217
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-26 22:54:31 +0500

    main

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { IframUrl, altval } from '../../constant/consts'
+import  Iframe  from "react-iframe"
 // css
 import styles from "./Footer.module.css"
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className={styles.Footer}>
+Footer
+    </footer>
   )
 }
 
--- src/constant/consts.jsx
+++ src/constant/consts.jsx
@@ -1,1 +1,2 @@
 export const altval="undefined"
+export const IframUrl="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3426.1407749036352!2d73.4724845!3d30.826721349999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391809f8bd500929%3A0x8f56f795aca4f1ee!2sChak%201%2F4%20L%2C%20Okara%2C%20Punjab!5e0!3m2!1sen!2s!4v1706290890598!5m2!1sen!2s"
